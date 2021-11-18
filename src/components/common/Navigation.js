a1c10b419366f098d94e99c4ce561747a04ed4df
maxicordoba <maxi_cordoba32@hotmail.com>
2021-11-18 00:57:10 -0300

    se creo la nueva estructura del proyecto

---
+++ src/components/common/Navigation.js
@@ -0,0 +1,10 @@
+<Navbar bg="primary" variant="dark">
+    <Container>
+    <Navbar.Brand href="#home">CRUD Cafe</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Inicio</Nav.Link>
+      <Nav.Link href="#features">Productos</Nav.Link>
+      <Nav.Link href="#pricing">Extra</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
